9a1df1253f9c174b2e06447c8b5389aef115626e
Bruno Mazzo <mazzo.bruno@gmail.com>
2016-11-25 21:20:01 -0200

    Initial commit

---
+++ iOS Project/Danger-Slather/Danger-Slather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Danger-Slather
+//
+//  Created by Bruno Mazzo on 11/25/16.
+//  Copyright © 2016 Bruno Mazzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
